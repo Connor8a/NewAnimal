0bda43469cdb2adecac0b3b95aacf7e31028837b
Connor8a <connorochoa@mail.weber.edu>
2022-12-06 17:50:30 -0700

    First bits

---
+++ animals.py
@@ -0,0 +1,18 @@
+# Class is a blueprint of an object 
+class Animal():
+    #constructor -- initializer -- defining attrivutes of the class
+    def _init_(self, name, color, species):
+        self.name = name
+        self.color = color
+        self.species = species
+    
+    def sound(): 
+        print("I don't make a sound. ")
+
+class Bird(Animal):
+    pass
+
+cat = Animal("felix", "brown", "feline")
+dog = Animal("rover", "golden", "canine")
+bird = Animal("Tweety", "yellow", "avian")
+fish = Animal("Nemo", "orange", "clown fish")
